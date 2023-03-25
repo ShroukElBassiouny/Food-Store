a007543efc7707da47bd81ab8ef434db86cbdae7
ShroukElBassiouny <shroukelbassiony@gmail.com>
2023-03-26 01:49:04 +0200

    creat listing food

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { food } from 'src/app/shared/models/food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent{
+  foods:food[] = [];
+  constructor(private foodService:FoodService){
+    this.foods = foodService.getAll()
+  }
+
+}
